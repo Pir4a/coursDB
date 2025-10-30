5bbca8b0ee35c1612e3df0521ba8cdbd4885edaf
Stephane Dedu <stephane.dedu@swapn.fr>
2025-10-30 12:01:52 +0100

    feat(model): add Article schema with validation, methods, statics and virtuals

--- schemas/Articles.js
+++ schemas/Articles.js
@@ -35,8 +35,54 @@ const articleSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
-});
+}, { timestamps: true, toJSON: { transform: (doc, ret) => {
+  ret.id = ret._id;
+  delete ret._id;
+  delete ret.__v;
+  return ret;
+} } });
 
+articleSchema.methods.publier = function() {
+  this.publie = true;
+  return this.save();
+};
+articleSchema.methods.despublier = function() {
+  this.publie = false;
+  return this.save();
+};
+articleSchema.methods.incrementVues = function() {
+  this.vues++;
+  return this.save();
+};
+
+// static methods
+articleSchema.statics.findAll = async function() {
+  return this.find().sort({ createdAt: -1 });
+};
+articleSchema.statics.findByCategorie = async function(categorie) {
+  return this.find({ categorie }).sort({ createdAt: -1 });
+};
+articleSchema.statics.findPublie = async function() {
+  return this.find({ publie: true }).sort({ createdAt: -1 });
+};
+articleSchema.statics.findArticleById = async function(id) {
+  return this.findById(id);
+};
+articleSchema.statics.updateArticleById = async function(id, data) {
+  return this.findByIdAndUpdate(id, data, { new: true });
+};
+articleSchema.statics.deleteArticleById = async function(id) {
+  return this.findByIdAndDelete(id);
+};
+
+
+
+// virtuals
+articleSchema.virtual('resume').get(function() {
+    if (this.contenu.length > 100) return this.contenu; 
+
+  return this.contenu.substring(0, 100) + '...';
+});
 const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
